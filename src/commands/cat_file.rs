13ff10bffe6f1a66815d88317618df248a9b5132
Erik Schierboom <erik_schierboom@hotmail.com>
2024-11-06 20:49:55 +0100

    cat: initial setup

---
+++ src/commands/cat_file.rs
@@ -0,0 +1,3 @@
+pub(crate) fn cat(sha: String) {
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use clap::{Parser, Subcommand};
+use clap::{Parser, Subcommand, ValueEnum};
 
 mod commands;
 
@@ -16,12 +16,19 @@ enum Command {
         /// The directory in which to create the Git repository
         #[arg(index = 1, default_value = ".")]
         directory: std::path::PathBuf
+    },
+    /// Output a file
+    CatFile {
+        /// The SHA of the file
+        #[arg(index = 1)]
+        sha: String
     }
 }
 
 fn main() {
     let args = Args::parse();
     match args.command {
-        Command::Init { directory}  => commands::init::init_repository(directory)
+        Command::Init { directory}  => commands::init::init_repository(directory),
+        Command::CatFile { sha}  => commands::cat_file::cat(sha),
     }
 }
