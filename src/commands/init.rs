5bafb2bb1bc6a8e48ad4814af7f2a06f442b7cb3
Erik Schierboom <erik_schierboom@hotmail.com>
2024-11-06 17:16:16 +0100

    init: basic version

--- src/commands/init.rs
+++ src/commands/init.rs
@@ -1,3 +1,11 @@
-pub(crate) fn init_repository() {
+use std::{fs, env};
 
+// TODO: replace expect/panic! with returning custom error
+
+pub(crate) fn init_repository() {
+    let working_dir = env::current_dir().expect("ERROR: could not get current directory");
+    let git_dir = working_dir.join(".git");
+    if git_dir.exists() { panic!("ERROR: the .git directory already exists") }
+
+    fs::create_dir(git_dir).expect("ERROR: could not create .git directory")
 }
