a82c223ba071382d782c165ad7eb21e2525f800b
Erik Schierboom <erik_schierboom@hotmail.com>
2024-11-08 16:16:46 +0100

    test: add hash-object"

--- src/commands/hash_object.rs
+++ src/commands/hash_object.rs
@@ -20,11 +20,16 @@ pub(crate) fn execute(write: bool, file: PathBuf, object_type: ObjectType) {
     let hash_bytes = hasher.finalize();
     let hash = format!("{:x}", hash_bytes);
 
-    let x = format!(".git/objects/{}/{}", hash[0..2].to_string(), hash[2..].to_string());
-    let output_file = Path::new(&x);
+    let dir_path = format!(".git/objects/{}", &hash[0..2]);
+    let dir = Path::new(&dir_path[0..]);
+    let output_file = dir.join(hash[2..].to_string());
 
     if write {
-        let f = File::open(output_file).expect("ERROR: could not write hash to file");
+        if !dir.exists() {
+            fs::create_dir(dir).expect("could not create dir")
+        }
+
+        let f = File::create(output_file).expect("ERROR: could not write hash to file");
         let mut encoder = ZlibEncoder::new(f, Compression::none());
         encoder.write_all(&hash_bytes).expect("ERROR: could not write hash to file");
     }
--- tests/hash_object_command_test.rs
+++ tests/hash_object_command_test.rs
@@ -30,3 +30,16 @@ fn test_hash_object_without_write_flag_and_non_existing_file_fails() {
     let status = command.status().expect("whoops");
     assert!(!status.success());
 }
+
+#[test]
+fn test_hash_object_with_write_flag_outputs_object() {
+    let binary = Binary::build();
+    let temp_dir = TempDir::new();
+
+    fs::write(temp_dir.path.join("test.txt"), "hello world").expect("failed to write test file");
+    temp_dir.command("git", ["init"]).status().expect("failed to run 'git init' command");
+
+    let mut command = temp_dir.command(binary.path.to_str().unwrap(), ["hash-object", "-w", "test.txt"]);
+    assert!(command.status().expect("failed to get status").success());
+    assert!(fs::exists(temp_dir.path.join(".git/objects/95/d09f2b10159347eece71399a7e2e907ea3df4f")).expect("whoops"))
+}
