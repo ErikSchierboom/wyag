d68a2adf68c77349c101fecdffcfe24b878b70a2
Erik Schierboom <erik_schierboom@hotmail.com>
2024-11-08 10:33:50 +0100

    tests: restructure

--- tests/add_command_test.rs
+++ tests/add_command_test.rs
@@ -1,26 +1,8 @@
-use std::{env, path};
-use std::process::Command; // Run programs
+use crate::common::Binary;
 
-fn target_dir() -> path::PathBuf {
-    env::current_exe()
-        .ok()
-        .map(|mut path| {
-            path.pop();
-            if path.ends_with("deps") {
-                path.pop();
-            }
-            path
-        })
-        .expect("this should only be used where a `current_exe` can be set")
-}
+mod common;
 
 #[test]
-fn test_add() {
-    Command::new("cargo").arg("build").status().expect("Cannot build");
-    let binary = target_dir().join(format!("{}{}", env!("CARGO_PKG_NAME"), env::consts::EXE_SUFFIX));
-
-
-    // todo!("qweeq")
-
-    let mut cmd = Command::new(binary).arg("init").status().expect("Should succeed");
+fn test_init() {
+    Binary::new().run(["init"]).expect("should succeed");
 }
